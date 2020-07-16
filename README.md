d048aa94869eecb33fe6f6f8c69bf5e4a171c41e
ido <ido.roseman@gmail.com>
2020-07-17 00:42:10 +0300

    added some documentation

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # dj-midi-controller
 
 this is a project to build a dj style controller for use with SDR-Console softare used by ham radio operators
-
+![image here](doc/isometric.png)
+## data files
 there are three folders :
 - eagle for the pcb design
 - arduino for the firmware
-- openscad for the encloser
+- 3d for the encloser
+
+## sdr-console configuration
+you can open the midi settings and set each fucntion as you like, or use my presets
+
+## Button Functions
+![image here](doc/front.png)
+encoder 1 is the frequency tuning
+if you press button 2 (the odd shaped one) it will tune at 10x speed
+encoder 2 is not currently used 
+encoder 3 is volume control, if you press it - it will mute the reciever
+button 1 toggles RX/TX
+
+if you want to change button2 to a normal seperate function, check the arduino code
+
 
 
--- arduino/sdr_midi_controller/sdr_midi_controller.ino
+++ arduino/sdr_midi_controller/sdr_midi_controller.ino
@@ -4,10 +4,12 @@
 // Instantiate a MIDI over USB interface.
 USBMIDI_Interface midi;
 
+// I use banks to alternate enc1 between normal & 10x tune
+// bank is selected by button5 (see below)
 Bank<2> bank(1); // A bank with four channels, and 2 bank settings
  
 // Instantiate a CCRotaryEncoder object
-                        // pins // MIDI address // multiplier
+//                        pins // MIDI address // multiplier
 Bankable::CCRotaryEncoder enc1 = { {bank, BankType::CHANGE_CHANNEL}, {A0, 14}, MCU::V_POT_1,  1, };
 CCRotaryEncoder enc2 = { {4, 3}, MCU::V_POT_2,  1, };
 CCRotaryEncoder enc3 = { {7, 6}, MCU::V_POT_3,  1, };
@@ -20,6 +22,7 @@ CCButton button4 = { 8, {MIDI_CC::General_Purpose_Controller_4, CHANNEL_1}, };
 SwitchSelector selector = {bank, 9};
 
 void setup() {
+  // this is needed for my hardware, where enc1 gets ground signal from pin 15 of arudino 
   pinMode(15, OUTPUT);
   digitalWrite(15, LOW);
 
